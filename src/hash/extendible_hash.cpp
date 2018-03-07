d3e63c9ef5c5f96565c2b9a4bc543dc9cd332648
Handora <qcdsr970209@gmail.com>
2018-03-08 01:04:03 +0800

    starting extendible hash

--- src/hash/extendible_hash.cpp
+++ src/hash/extendible_hash.cpp
@@ -10,7 +10,10 @@ namespace cmudb {
  * array_size: fixed array size for each bucket
  */
 template <typename K, typename V>
-ExtendibleHash<K, V>::ExtendibleHash(size_t size) {}
+ExtendibleHash<K, V>::ExtendibleHash(size_t size) {
+  this->size_ = size;
+  
+}
 
 /*
  * helper function to calculate the hashing address of input key
@@ -33,45 +36,46 @@ int ExtendibleHash<K, V>::GetGlobalDepth() const {
  * helper function to return local depth of one specific bucket
  * NOTE: you must implement this function in order to pass test
  */
-  template <typename K, typename V>
-  int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
-    return 0;
-  }
+template <typename K, typename V>
+int ExtendibleHash<K, V>::GetLocalDepth(int bucket_id) const {
+  return 0;
+}
+  
+/*
+ * helper function to return current number of bucket in hash table
+ */
+template <typename K, typename V>
+int ExtendibleHash<K, V>::GetNumBuckets() const {
+  return 0;
+}
 
-  /*
-   * helper function to return current number of bucket in hash table
-   */
-  template <typename K, typename V>
-  int ExtendibleHash<K, V>::GetNumBuckets() const {
-    return 0;
-  }
+/*
+ * lookup function to find value associate with input key
+ */
+template <typename K, typename V>
+bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
+  return false;
+}
+  
+/*
+ * delete <key,value> entry in hash table
+ * Shrink & Combination is not required for this project
+ */
+template <typename K, typename V>
+bool ExtendibleHash<K, V>::Remove(const K &key) {
+  return false;
+}
+  
 
-  /*
-   * lookup function to find value associate with input key
-   */
-  template <typename K, typename V>
-  bool ExtendibleHash<K, V>::Find(const K &key, V &value) {
-    return false;
-  }
+/*
+ * insert <key,value> entry in hash table
+ * Split & Redistribute bucket when there is overflow and if necessary increase
+ * global depth
+ */
+template <typename K, typename V>
+void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {}
 
-  /*
-   * delete <key,value> entry in hash table
-   * Shrink & Combination is not required for this project
-   */
-  template <typename K, typename V>
-  bool ExtendibleHash<K, V>::Remove(const K &key) {
-    return false;
-  }
-
-  /*
-   * insert <key,value> entry in hash table
-   * Split & Redistribute bucket when there is overflow and if necessary increase
-   * global depth
-   */
-  template <typename K, typename V>
-  void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {}
-
-  template class ExtendibleHash<page_id_t, Page *>;
+template class ExtendibleHash<page_id_t, Page *>;
 template class ExtendibleHash<Page *, std::list<Page *>::iterator>;
 // test purpose
 template class ExtendibleHash<int, std::string>;
--- src/include/hash/extendible_hash.h
+++ src/include/hash/extendible_hash.h
@@ -17,23 +17,26 @@
 
 namespace cmudb {
 
-template <typename K, typename V>
-class ExtendibleHash : public HashTable<K, V> {
-public:
-  // constructor
-  ExtendibleHash(size_t size);
-  // helper function to generate hash addressing
-  size_t HashKey(const K &key);
-  // helper function to get global & local depth
-  int GetGlobalDepth() const;
-  int GetLocalDepth(int bucket_id) const;
-  int GetNumBuckets() const;
-  // lookup and modifier
-  bool Find(const K &key, V &value) override;
-  bool Remove(const K &key) override;
-  void Insert(const K &key, const V &value) override;
-
-private:
+  template <typename K, typename V>
+    class ExtendibleHash : public HashTable<K, V> {
+  public:
+    // constructor
+    ExtendibleHash(size_t size);
+    // helper function to generate hash addressing
+    size_t HashKey(const K &key);
+    // helper function to get global & local depth
+    int GetGlobalDepth() const;
+    int GetLocalDepth(int bucket_id) const;
+    int GetNumBuckets() const;
+    // lookup and modifier
+    bool Find(const K &key, V &value) override;
+    bool Remove(const K &key) override;
+    void Insert(const K &key, const V &value) override;
+    
+  private:
   // add your own member variables here
-};
+    size_t size_;
+    int global_depth_ = 0;
+    int num_buckets = 0; 
+  };
 } // namespace cmudb
